0b48b8e2c57dc050311128bd217faefae8d44e6a
Bhagyashree Kane <131791462+BhagyashreeMallikarjunKane@users.noreply.github.com>
2024-07-24 20:09:05 +0530

    Add files via upload

---
+++ my math operations/md.js
@@ -0,0 +1,45 @@
+﻿function addNumber(){
+                var num1 = parseInt(document.getElementById("text1").value);
+                var num2 = parseInt(document.getElementById("text2").value);
+
+                var sum = num1+num2;
+          
+                document.getElementById("res").value = sum;  
+                             
+                }
+
+
+
+function subNumber(){
+                var num1 = parseInt(document.getElementById("text1").value);
+                var num2 = parseInt(document.getElementById("text2").value);
+
+
+    var sub = num1-num2;
+
+    document.getElementById("res").value = sub;  
+}
+
+
+
+
+function mulNumber(){
+    var num1 = parseInt(document.getElementById("text1").value);
+    var num2 = parseInt(document.getElementById("text2").value);
+
+    var mul = num1*num2;
+
+    document.getElementById("res").value = mul;  
+}
+
+
+
+
+function divNumber(){
+    var num1 = parseInt(document.getElementById("text1").value);
+    var num2 = parseInt(document.getElementById("text2").value);
+
+    var div = num1/num2;
+   
+document.getElementById("res").value = div; 
+}
